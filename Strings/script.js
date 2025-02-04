98c0c63459e921454299ec2dd6a44e5db616c73e
Asad <asadalipuh5@gmail.com>
2025-02-05 00:47:56 +0500

    Strings

---
+++ Strings/script.js
@@ -0,0 +1,26 @@
+console.log("Strings")
+
+let name = "Asad"
+
+console.log(name[0])
+console.log(name[1])
+console.log(name[2])
+console.log(name[3])
+
+console.log(name.length)
+
+let friend = "Mustafa"
+
+console.log("his name is: " + name, "and his firends name is: " + friend)
+console.log(`His name is ${name} and his friends name is ${friend}`)
+
+let b = "zia"
+
+console.log(b.toUpperCase())
+console.log(b.slice(0,2))
+console.log(b.slice(1))
+
+console.log(b.replace("zia", "shahzad"))
+console.log(b.concat(name, "Panhwar"))
+
+console.log(b.charAt(0))
