9d95abb808c09feacd8e4899d3dc3eaf2f160e8e
Asad <asadalipuh5@gmail.com>
2025-02-05 23:27:00 +0500

    Selecting by Ids, class and more

--- Selecting by Ids class/index.js
+++ Selecting by Ids class/index.js
@@ -1,1 +1,30 @@
 console.log("Asad")
+
+let boxes = document.body.getElementsByClassName("box")
+
+console.log(boxes)
+console.log(boxes[2])
+
+boxes[2].style.backgroundColor = "red"
+
+let boxes_1 = document.getElementById("two")
+
+console.log(boxes_1)
+
+boxes_1.style.backgroundColor = "black"
+
+document.querySelector(".box").style.backgroundColor = "green";
+
+let allElements = document.querySelectorAll(".box")
+
+console.log(allElements)
+
+allElements.forEach(e=>{
+    e.style.backgroundColor = "green"
+})
+
+console.log(document.getElementsByTagName("div"))
+
+let e = document.getElementsByTagName("div")
+
+console.log(e[1].matches("onebox"))
